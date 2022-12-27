b058e5427b107a9c235eb700e5b805d2e0bc90f6
Timo Schaller <contact@timo-schaller.de>
2022-12-27 11:07:37 +0100

    Removing old files

--- scripts/List_AD_Computer.ps1
+++
@@ -1,3 +0,0 @@
-Get-ADComputer -Filter {Name -like "*NAME_SCHEME*" -and Enabled -eq $True } | select Name | Sort-Object Name | ft
-$Shell = New-Object -ComObject "WScript.Shell"
-$Button = $Shell.Popup("Click OK to continue.", 0, "Waiting to exit...", 0){\rtf1}
